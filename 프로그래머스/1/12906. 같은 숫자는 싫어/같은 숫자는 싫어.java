0a52ad88999cfe5351e2aac18aba2105a8f518a7
Jiwon Jung <134471035+jionnie@users.noreply.github.com>
2026-07-29 17:05:56 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 22.31 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -5,30 +5,20 @@ import java.util.*;
  * 1. 0 ~ 9 사이의 정수로 이루어진 배열 arr가 주어진다.
  * 2. arr에서 연속적으로 숫자가 나타난다면 하나만 남기고 전부 제거한다.
  * 3. 단, 제거된 후 남은 수들의 숫자들은 원래 순서를 유지해야 한다.
+ * 4. 가장 나중에 들어간 데이터를 다루는 문제이므로 스택을 사용해서 풀 수 있다.
+ * 5. 하지만 스택까지도 사용할 필요 없이 배열을 순회해서 풀 수 있다.
  */
 public class Solution {
     public int[] solution(int[] arr) {
-        Deque<Integer> stack = new ArrayDeque<>();
         List<Integer> answer = new ArrayList<>();
 
-        for (int i = arr.length - 1; i >= 0; i--) {
-            stack.push(arr[i]);
-        }
-
         int prev = -1;
-        int size = stack.size();
-        
-        for (int i = 0; i < size; i++) {
-            int now = stack.pop();
 
-            if (prev == now) {
+        for (int now : arr) {
+            if (prev != now) {
+                answer.add(now);
                 prev = now;
-                continue;
             }
-
-            answer.add(now);
-
-            prev = now;
         }
 
         return answer.stream().mapToInt(Integer::intValue).toArray();
