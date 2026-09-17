1cf8d6a79e97b4e044a2f1ae037260293b06caf8
Jiwon Jung <134471035+jionnie@users.noreply.github.com>
2026-09-17 12:49:23 +0900

    [level 1] Title: 특정 형질을 가지는 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/301646. 특정 형질을 가지는 대장균 찾기/특정 형질을 가지는 대장균 찾기.sql
+++ 프로그래머스/1/301646. 특정 형질을 가지는 대장균 찾기/특정 형질을 가지는 대장균 찾기.sql
@@ -1,3 +1,6 @@
-SELECT COUNT(*) AS COUNT
+-- 2번 형질이 보유하지 않으면서 = GENOTYPE & 2 = 0 → 2번 형질 없음
+-- 1번 or 3번 형질을 보유하고 있는 대장균 개체의 수 = (GENOTYPE & 1) <> 0 OR (GENOTYPE & 4) <> 0
+-- 3번 형질 = 4 인 이유는 2진수에서 “세 번째 비트 자리값”이 4이기 때문
+SELECT COUNT(*) AS COUNT 
 FROM ECOLI_DATA
 WHERE GENOTYPE & 2 = 0 AND (GENOTYPE & 1 <> 0 OR GENOTYPE & 4 <> 0)
